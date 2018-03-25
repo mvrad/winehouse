a6172902ce0d4035a07fcfd33b5998fd78299a32
Matthew Conrad <matthew.conrad@outlook.com>
2018-03-25 04:02:41 -0700

    Add setInterval function to app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express"),
   mongoose = require("mongoose"),
   session = require("express-session"),
   MongoStore = require("connect-mongo")(session),
+  http = require("http"),
   PORT = process.env.PORT || 3000,
-  MY_API_KEY = process.env.MY_API_KEY,
   app = express();
 
 // mongodb connection
@@ -65,3 +65,8 @@ app.use(function(err, req, res, next) {
 app.listen(PORT, function () {
   console.log(`Express app listening on port ${ PORT }`);
 });
+
+// Ping app every 5 minutes
+setInterval(function() {
+  http.get("http://winoapp.herokuapp.com");
+}, 300000);
