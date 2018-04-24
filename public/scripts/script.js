8e71ea47e4a43c3c613f104148c8e40c25e5e90e
Matthew Conrad <matthew.conrad@outlook.com>
2018-04-24 00:24:54 -0700

    Add option to append wine to collection

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,6 +2,7 @@ $(() => {
 
   "use strict";
 
+  // Globals
   const akey = config.KEY,
     snoothAPI = "https://api.snooth.com/wines/?akey=" + akey + "&q=";
 
@@ -13,56 +14,77 @@ $(() => {
       $(".register").animate({marginTop: "0"})
     } else {
       $(".header").animate({marginTop: "-295px"})
-      $(".register").animate({marginTop: "-295px"})     
+      $(".register").animate({marginTop: "-295px"})
     }
   });
 
-  // Set focus on the first sign up form input field
+  // Set focus to fields
   $("#name").focus();
-
-  // Set focus on the first sign in form input field
   $("#login-email input").focus();
-
-  // Set focus on the wine search input field
   $("#search-form input").focus();
 
   // Wine Search (powered by Snooth API)
   $("#search").keyup((e) => {
+
     let search = $("#search").val().toLowerCase().trim(),
       value = $.trim($("#search").val());
+
     if (value.length > 0) {
+
       let searching = snoothAPI + search;
+
       $(".search-dropdown").addClass("is-active");
+
       // Get Snooth API
       $.getJSON(searching, (data) => {
-          let wines = data.wines;
+
+        let wines = data.wines;
+
         $.each(wines, (i) => {
+
           let name = "<span class=labels>Name: </span>" + wines[0].name,
             varietal = "<span class=labels>Varietal: </span>" + wines[0].varietal,
             vintage = "<span class=labels>Vintage: </span>" + wines[0].vintage,
             type = "<span class=labels>Type: </span>" + wines[0].type,
             price = "<span class=labels>Price: </span>" + "$" + wines[0].price,
             img = "<img src=" + wines[0].image + ">",
-            newLink = img.replace("https://", "//");
+            imgURL = wines[0].image;
 
-            $(".search-dropdown-list__name").html(name);
-            $(".search-dropdown-list__varietal").html(varietal);
-            $(".search-dropdown-list__vintage").html(vintage);
-            $(".search-dropdown-list__type").html(type);
-            $(".search-dropdown-list__price").html(price);
-            $(".search-dropdown-list__image").html(newLink);
+          $(".search-dropdown-list__name").html(name);
+          $(".search-dropdown-list__varietal").html(varietal);
+          $(".search-dropdown-list__vintage").html(vintage);
+          $(".search-dropdown-list__type").html(type);
+          $(".search-dropdown-list__price").html(price);
+          $(".search-dropdown-list__image").html(img);
+          $(".search-dropdown-list__imgURL").html(imgURL);
 
-            // $("#add-to-collection").click(() => {
-            //   $(".collection-main").after(".collection-wine");
-            //   if ($(".collection-main").after(".collection-wine")) {
-            //     $("wine-type").append(varietal);
-            //     $("wine-year").append(vintage);
-            //   }
-            // });
         }); // End Wine Arr
+
       }); // End getJSON
+
     } else {
       $(".search-dropdown").removeClass("is-active");
     }
-  }); // End Click Event
+
+  }); // End Keyup Event
+
+  // Add Wine to Collection
+  $("#add-to-collection").click(() => {
+    let name = $(".search-dropdown-list__name").text(),
+      varietal = $(".search-dropdown-list__varietal").text(),
+      vintage = $(".search-dropdown-list__vintage").text(),
+      imgURL = $("#imgURL").text();
+
+    $(".collection-main").append(
+      "<div class=collection-wine>" + 
+      "<img class=wine-glass src=" + imgURL + ">" + 
+      "<span class=wine-name>" + name + "</span>" + 
+      "<span class=wine-type>" + varietal + "</span>" +  
+      "<span class=wine-year>" + vintage + "</span>" + 
+      "<img class=wine-note src='images/note.svg'" + ">" + 
+      "<div class=wine-delete>&times</div>" + 
+      "</div>"
+    );
+  });
+
 }); // End Doc Ready
