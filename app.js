928278af4dc8dfe18597ccfb419f8701a160a260
Matthew Conrad <matthew.conrad@outlook.com>
2020-10-28 14:19:15 -0700

    Fix .gitignore

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const scout = require("@scout_apm/scout-apm"),
   csp = require("helmet-csp"),
   referrerPolicy = require("referrer-policy"),
   expiryDate = new Date(Date.now() + 60 * 60 * 1000),
-<<<<<<< HEAD
   PORT = process.env.PORT || 3000;
 
 // Scout
@@ -117,97 +116,6 @@ async function start() {
   // Listen on port 3000
   app.listen(PORT, function () {
     console.log(`Express app listening on port ${ PORT }`);
-=======
-  PORT = process.env.PORT || 3000,
-  app = express();
-
-// Helmet
-app.use(helmet());
-
-// CSP middleware
-app.use(csp({
-  directives: {
-    upgradeInsecureRequests: true,
-    workerSrc: false
-  },
-  loose: false,
-  reportOnly: false,
-  setAllHeaders: true,
-  disableAndroid: false,
-  browserSniff: true
-}));
-
-// Referrer policy
-app.use(referrerPolicy());
-
-// Feature policy
-app.use(helmet.featurePolicy({
-  features: {
-    syncXhr: ["'none'"]
-  }
-}));
-
-// MongoDB connection
-const db = mongoose.connection;
-mongoose.connect("mongodb+srv://dbUser:dbUserPassword@cluster0.mwr2r.mongodb.net/winehouse?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useCreateIndex: true
-});
-// Mongo error
-db.on("error", console.error.bind(console, "connection error:"));
-
-// Use sessions for tracking logins
-app.set("trust proxy", 1);
-app.use(session({
-  secret: "Matt loves you",
-  name: "sessionId",
-  resave: true,
-  saveUninitialized: false,
-  store: new MongoStore({
-    mongooseConnection: db
-  }),
-  cookie: {
-    secure: true,
-    httpOnly: true,
-    maxAge: expiryDate
-  }
-}));
-
-// Make user ID available in templates
-app.use(function (req, res, next) {
-  res.locals.currentUser = req.session.userId;
-  next();
-});
-
-// Parse incoming requests
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Serve static files from /public
-app.use(express.static(__dirname + "/public"));
-
-// View engine setup
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-
-// Include routes
-const routes = require("./routes/index");
-app.use("/", routes);
-
-// Catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error("File Not Found");
-  err.status = 404;
-  next(err);
-});
-
-// Error handler
-// Define as the last app.use callback
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  return res.render("error", {
-    message: err.message
->>>>>>> 3400cdd1a19f2e9ec0c89eec9d746d4de9607605
   });
 
   app.start();
