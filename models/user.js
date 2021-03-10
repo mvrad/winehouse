5078f17a9ba4e60e687f58feac7de7fc889f77a0
Matthew Conrad <matthew.conrad@outlook.com>
2021-03-10 03:44:54 -0800

    Remove authentication with mongoose

--- models/user.js
+++ models/user.js
@@ -1,56 +1,56 @@
-const mongoose = require("mongoose"),
-  bcrypt = require("bcrypt"),
-  UserSchema = new mongoose.Schema({
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-      trim: true
-    },
-    name: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    password: {
-      type: String,
-      required: true
-    }
-  });
+// const mongoose = require("mongoose"),
+//   bcrypt = require("bcrypt"),
+//   UserSchema = new mongoose.Schema({
+//     email: {
+//       type: String,
+//       unique: true,
+//       required: true,
+//       trim: true
+//     },
+//     name: {
+//       type: String,
+//       required: true,
+//       trim: true
+//     },
+//     password: {
+//       type: String,
+//       required: true
+//     }
+//   });
 
-// Authenticate input against database documents
-UserSchema.statics.authenticate = function(email, password, callback) {
-  User.findOne({ email: email })
-    .exec(function (error, user) {
-      if (error) {
-        return callback(error);
-      } else if ( !user ) {
-        var err = new Error("User not found.");
-        err.status = 401;
-        return callback(err);
-      }
-      bcrypt.compare(password, user.password , function(error, result) {
-        if (result === true) {
-          return callback(null, user);
-        } else {
-          return callback();
-        }
-      })
-    });
-}
+// // Authenticate input against database documents
+// UserSchema.statics.authenticate = function(email, password, callback) {
+//   User.findOne({ email: email })
+//     .exec(function (error, user) {
+//       if (error) {
+//         return callback(error);
+//       } else if ( !user ) {
+//         var err = new Error("User not found.");
+//         err.status = 401;
+//         return callback(err);
+//       }
+//       bcrypt.compare(password, user.password , function(error, result) {
+//         if (result === true) {
+//           return callback(null, user);
+//         } else {
+//           return callback();
+//         }
+//       })
+//     });
+// }
 
-// Hash password before saving to database
-UserSchema.pre("save", function(next) {
-  let user = this;
-  bcrypt.hash(user.password, 10, function(err, hash) {
-    if (err) {
-      return next(err);
-    }
-    user.password = hash;
-    next();
-  })
-});
+// // Hash password before saving to database
+// UserSchema.pre("save", function(next) {
+//   let user = this;
+//   bcrypt.hash(user.password, 10, function(err, hash) {
+//     if (err) {
+//       return next(err);
+//     }
+//     user.password = hash;
+//     next();
+//   })
+// });
 
-let User = mongoose.model("User", UserSchema);
+// let User = mongoose.model("User", UserSchema);
 
-module.exports = User;
+// module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -5,97 +5,97 @@ const express = require("express"),
 
 // GET /collection
 router.get("/collection", function(req, res, next) {
-  User.findById(req.session.userId)
-    .exec(function (error, user) {
-      if (error) {
-        return next(error);
-      } else {
+  // User.findById(req.session.userId)
+    // .exec(function (error, user) {
+    //   if (error) {
+    //     return next(error);
+    //   } else {
         return res.render("collection", { title: "Collection", path: req.path });
-      }
-    });
+    //   }
+    // });
 });
 
 // GET /logout
 router.get("/logout", function(req, res, next) {
-  if (req.session) {
-    // delete session object
-    req.session.destroy(function(err) {
-      if(err) {
-        return next(err);
-      } else {
+  // if (req.session) {
+  //   // delete session object
+  //   req.session.destroy(function(err) {
+  //     if(err) {
+  //       return next(err);
+  //     } else {
         return res.redirect("/");
-      }
-    });
-  }
+  //     }
+  //   });
+  // }
 });
 
 // GET /login
-router.get("/login", mid.loggedOut, function(req, res, next) {
-  return res.render("login", { title: "Log In" });
-});
+// router.get("/login", mid.loggedOut, function(req, res, next) {
+//   return res.render("login", { title: "Log In" });
+// });
 
 // POST /login
-router.post("/login", function(req, res, next) {
-  if (req.body.email && req.body.password) {
-    User.authenticate(req.body.email, req.body.password, function (error, user) {
-      if (error || !user) {
-        const err = new Error("Wrong email or password.");
-        err.status = 401;
-        return next(err);
-      }  else {
-        req.session.userId = user._id;
-        return res.redirect("/collection");
-      }
-    });
-  } else {
-    const err = new Error("Email and password are required.");
-    err.status = 401;
-    return next(err);
-  }
-});
+// router.post("/login", function(req, res, next) {
+//   if (req.body.email && req.body.password) {
+//     User.authenticate(req.body.email, req.body.password, function (error, user) {
+//       if (error || !user) {
+//         const err = new Error("Wrong email or password.");
+//         err.status = 401;
+//         return next(err);
+//       }  else {
+//         req.session.userId = user._id;
+//         return res.redirect("/collection");
+//       }
+//     });
+//   } else {
+//     const err = new Error("Email and password are required.");
+//     err.status = 401;
+//     return next(err);
+//   }
+// });
 
 // GET /register
-router.get("/register", mid.loggedOut, function(req, res, next) {
-  return res.render("register", { title: "Sign Up" });
-});
+// router.get("/register", mid.loggedOut, function(req, res, next) {
+//   return res.render("register", { title: "Sign Up" });
+// });
 
 // POST /register
-router.post("/register", function(req, res, next) {
-  if (req.body.email &&
-    req.body.name &&
-    req.body.password &&
-    req.body.confirmPassword) {
+// router.post("/register", function(req, res, next) {
+//   if (req.body.email &&
+//     req.body.name &&
+//     req.body.password &&
+//     req.body.confirmPassword) {
 
-      // confirm that user typed same password twice
-      if (req.body.password !== req.body.confirmPassword) {
-        const err = new Error("Passwords do not match.");
-        err.status = 400;
-        return next(err);
-      }
+//       // confirm that user typed same password twice
+//       if (req.body.password !== req.body.confirmPassword) {
+//         const err = new Error("Passwords do not match.");
+//         err.status = 400;
+//         return next(err);
+//       }
 
-      // create object with form input
-      var userData = {
-        email: req.body.email,
-        name: req.body.name,
-        password: req.body.password
-      };
+//       // create object with form input
+//       var userData = {
+//         email: req.body.email,
+//         name: req.body.name,
+//         password: req.body.password
+//       };
 
-      // use schema"s `create` method to insert document into Mongo
-      User.create(userData, function (error, user) {
-        if (error) {
-          return next(error);
-        } else {
-          req.session.userId = user._id;
-          return res.redirect("/collection");
-        }
-      });
+//       // use schema"s `create` method to insert document into Mongo
+//       User.create(userData, function (error, user) {
+//         if (error) {
+//           return next(error);
+//         } else {
+//           req.session.userId = user._id;
+//           return res.redirect("/collection");
+//         }
+//       });
 
-    } else {
-      const err = new Error("All fields required.");
-      err.status = 400;
-      return next(err);
-    }
-})
+//     } else {
+//       const err = new Error("All fields required.");
+//       err.status = 400;
+//       return next(err);
+//     }
+// })
 
 // GET /
 router.get("/", function(req, res, next) {
